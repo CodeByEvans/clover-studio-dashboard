fab93018d2dcfbddbec63af50272cd78061f655d
EvansJLR <evansjavierlr@gmail.com>
2025-11-26 22:07:01 +0100

    feat: supabase api created

---
+++ src/utils/supabase/api.ts
@@ -0,0 +1,30 @@
+import { createClient } from "@/lib/supabase/server";
+import { Database } from "@/types/supabase";
+
+type ProductInsert = Database["public"]["Tables"]["products"]["Insert"];
+
+type ProductRow = Database["public"]["Tables"]["products"]["Row"];
+type Products = ProductRow[];
+
+export const getProducts = async (): Promise<Products> => {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from<"products", ProductRow>("products")
+    .select("*");
+
+  if (error) throw error;
+
+  return data;
+};
+
+export const uploadProduct = async (product: ProductInsert) => {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from<"products", ProductInsert>("products")
+    .insert(product as any)
+    .select();
+
+  if (error) throw error;
+
+  return data;
+};
