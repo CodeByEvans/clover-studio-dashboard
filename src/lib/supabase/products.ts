99fd1a1bfcb47720df9db1520209216fe19dbdab
EvansJLR <evansjavierlr@gmail.com>
2025-11-27 03:01:18 +0100

    refactor: reorganize products service from supabase

--- src/lib/supabase/products.ts
+++ src/lib/supabase/products.ts
@@ -1,4 +1,5 @@
 import { createClient } from "@/lib/supabase/server";
+import { Product } from "@/types/product.type";
 import { Database } from "@/types/supabase";
 
 type ProductInsert = Database["public"]["Tables"]["products"]["Insert"];
@@ -17,11 +18,39 @@ export const getProducts = async (): Promise<Products> => {
   return data;
 };
 
+export const getProduct = async (id: number): Promise<Product> => {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from("products")
+    .select("*")
+    .eq("id", id);
+
+  if (error) throw error;
+
+  return data[0];
+};
+
 export const uploadProduct = async (product: ProductInsert) => {
   const supabase = await createClient();
   const { data, error } = await supabase
     .from<"products", ProductInsert>("products")
     .insert(product as any)
+    .select();
+
+  if (error) throw error;
+
+  return data;
+};
+
+export const updateProduct = async (
+  updatedProduct: ProductInsert,
+  id: number
+) => {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from("products")
+    .update(updatedProduct)
+    .eq("id", id)
     .select();
 
   if (error) throw error;
