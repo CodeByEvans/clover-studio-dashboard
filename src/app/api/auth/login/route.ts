8bdaef876f898f81063ca662e9b6e286ab0b9d3e
EvansJLR <evansjavierlr@gmail.com>
2025-11-28 19:25:56 +0100

    feat: auth api endpoint

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,22 @@
+import { createClient } from "@/lib/supabase/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { email, password } = await req.json();
+
+    const supabase = await createClient();
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      return NextResponse.json({ message: error.message }, { status: 400 });
+    }
+
+    return NextResponse.json(data.session, { status: 200 });
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
