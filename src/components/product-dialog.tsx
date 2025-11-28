12a2d0a8832368569574ace4410f46f58efa0eb1
EvansJLR <evansjavierlr@gmail.com>
2025-11-28 19:17:33 +0100

    feat: add product-add-form.tsx

--- src/components/product-dialog.tsx
+++ src/components/product-dialog.tsx
@@ -14,21 +14,38 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
-import { Product, ProductFormData } from "@/types/product.type";
+import { Products } from "@/types/product.type";
 import slugify from "slugify";
+import { productAPI } from "@/services/api";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { SelectGroup, SelectLabel } from "@radix-ui/react-select";
+import { Collections } from "@/types/collection.type";
 
 interface ProductDialogProps {
   open: boolean;
   onOpenChange: (open: boolean) => void;
+  setProducts: React.Dispatch<React.SetStateAction<Products>>;
+  collections: Collections;
 }
 
-export function ProductDialog({ open, onOpenChange }: ProductDialogProps) {
+export function ProductDialog({
+  open,
+  onOpenChange,
+  setProducts,
+  collections,
+}: ProductDialogProps) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [price, setPrice] = useState("");
+  const [price, setPrice] = useState<string>("");
   const [images, setImages] = useState<File[]>([]);
   const [portrait, setPortrait] = useState<File | null>(null);
-  const [collection, setCollection] = useState<number | string>("");
+  const [collection, setCollection] = useState<string>("");
 
   const handleCloseDialog = () => {
     setTitle("");
@@ -46,7 +63,10 @@ export function ProductDialog({ open, onOpenChange }: ProductDialogProps) {
     const images = e.target.files;
 
     setImages([...images]);
-    if (!portrait && images.length > 0) setPortrait(images[0]);
+
+    if (images.length > 0) {
+      setPortrait(images[0]);
+    }
   };
 
   const handleSetPortrait = (e: React.MouseEvent, img: File) => {
@@ -59,16 +79,32 @@ export function ProductDialog({ open, onOpenChange }: ProductDialogProps) {
     if (portrait === img) setPortrait(images[0]);
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    const product = {
-      title,
-      description,
-      price,
-      images,
-      portrait,
-      collection,
-    };
+    if (portrait === null) return;
+
+    const slug = slugify(title, { lower: true });
+
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("slug", slug);
+    formData.append("description", description);
+    formData.append("price", price.toString());
+    formData.append("collection", collection);
+    formData.append("featured", "false");
+
+    formData.append("portrait", portrait);
+
+    images.forEach((file) => formData.append("images", file));
+
+    try {
+      const response = await productAPI.createProduct(formData);
+      const product = response;
+      setProducts((prevProducts) => [...prevProducts, product]);
+      handleCloseDialog();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -83,7 +119,7 @@ export function ProductDialog({ open, onOpenChange }: ProductDialogProps) {
 
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
-            <Label htmlFor="title">Nombre del Producto *</Label>
+            <Label htmlFor="title">Nombre del Producto</Label>
             <Input
               id="title"
               placeholder="Ej: Flores Frescas"
@@ -94,7 +130,7 @@ export function ProductDialog({ open, onOpenChange }: ProductDialogProps) {
           </div>
 
           <div>
-            <Label htmlFor="description">Descripción *</Label>
+            <Label htmlFor="description">Descripción</Label>
             <Textarea
               id="description"
               placeholder="Describe tu producto..."
@@ -106,7 +142,7 @@ export function ProductDialog({ open, onOpenChange }: ProductDialogProps) {
           </div>
 
           <div>
-            <Label htmlFor="price">Precio *</Label>
+            <Label htmlFor="price">Precio</Label>
             <Input
               id="price"
               type="number"
@@ -160,13 +196,27 @@ export function ProductDialog({ open, onOpenChange }: ProductDialogProps) {
 
           <div>
             <Label htmlFor="collection">ID de la Colección</Label>
-            <Input
-              id="collection"
-              placeholder="Escribe el ID de la colección"
+            <Select
               value={collection}
-              required
-              onChange={(e) => setCollection(e.target.value)}
-            />
+              onValueChange={(value) => setCollection(value)}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Selecciona una colección" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Colecciones</SelectLabel>
+                  {collections.map((collection) => (
+                    <SelectItem
+                      key={collection.id}
+                      value={collection.id.toString()}
+                    >
+                      {collection.title}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
           </div>
 
           <DialogFooter>
